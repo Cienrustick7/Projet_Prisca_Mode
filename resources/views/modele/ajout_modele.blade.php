ec76affb02262d3d2d966c8c10d0fba6d2e4607f
stevie Nguila <stevierosenguila2023@gmail.com>
2023-09-08 11:29:17 +0100

    recuperation de donnee

--- resources/views/modele/ajout_modele.blade.php
+++ resources/views/modele/ajout_modele.blade.php
@@ -18,6 +18,7 @@
                 <h1 class="text-center">Nouveau modeles</h1>
                 <div class="container mt-2" style="width: 20%">
                     <a href="{{ Route('form_modele') }}" class="btn btn-danger">Ajouter</a>
+
                 </div>
                 <div class="container">
                     <div class="row row-cols-1 row-cols-md-2 g-4">
